4e2166d465a504617f85508918b20c1015c4a0b3
yeongrong-heo <102986339+yeongr1203@users.noreply.github.com>
2022-07-11 21:44:14 +0900

    Add files via upload

---
+++ vue.js/vue-project/public/components/FavoriteSinger.vue
@@ -0,0 +1,18 @@
+<template>
+    <img v-bind:src="imgSrc">   
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            imgSrc: 'https://post-phinf.pstatic.net/MjAyMTAxMjFfMTA3/MDAxNjExMTk2ODM3NzU4.er3X53Mr5Zcn0ynp1B5QAIWeyjwgk9T-1cSC7Tgqe14g.cLCKopgCQAUtv9y2wMvX1hxLlN1cZTezylgCCppn0X8g.PNG/%EA%B6%8C%EC%A0%95%EC%97%B4_10cm_playlist.png?type=w1200'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ vue.js/vue-project/public/components/todo/TodoList.vue
@@ -0,0 +1,48 @@
+<template>
+    <section>
+        <transition-group name="list" tag="ul">
+            <li :key="todoItem.key" v-for="todoItem in propsItems" class="shadow">
+                <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+                {{ todoItem.value }}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem.key)">
+                    <i class="far fa-trash-alt" aria-hidden="true"></i>
+                </span>
+            </li>
+        </transition-group>
+    </section>
+</template>
+
+<script>
+export default {
+    props: { propsItems: Array },
+    methods: {
+        removeTodo(key) { 
+            this.$emit('childDelTodo',key);
+        }
+        
+    }, 
+
+}
+</script>
+
+<style scoped>
+
+    .list-enter-active, .list-leave-active { transition: all 1s; }
+    .list-enter-from, .list-leave-to {
+        opacity: 0; transform: translateY(-30px);
+    }
+
+    ul { 
+        list-style-type: none; padding-left: 0; margin-top: 0; text-align: left;
+    }
+    li { 
+        display: flex; min-height: 50px; height: 50px; line-height: 50px; margin: 0.5rem 0; padding: 0 0.9rem;
+        background-color: white; border-radius: 5px;
+    }
+    .checkBtn { 
+        line-height: 50px; color: #62acde; margin-right: 5px;
+    }
+    .removeBtn { 
+        margin-left: auto; color: #de4343;
+    }
+</style>
