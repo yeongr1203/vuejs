4b6bb01200db55e3476d212035deccf01359716b
yeongrong-heo <102986339+yeongr1203@users.noreply.github.com>
2022-07-12 20:08:35 +0900

    Add files via upload

--- vue.js/vue-project/vue.config.js
+++ vue.js/vue-project/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  // 아래부터 내가 작성한 것 => devServer!! 상단 true까지는 기본! 
+  devServer: {  // devServer와 통신되도록 proxy설정을 위해 작성됨.
+    proxy: {
+      '/todo' : {
+        target: 'http://localhost'
+      }
+    }
+  }
+});
