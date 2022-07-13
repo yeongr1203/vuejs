c24460b669784779b3f5e9cf55c846c4c9976900
yeongrong-heo <102986339+yeongr1203@users.noreply.github.com>
2022-07-13 19:09:09 +0900

    20220713

---
+++ vue.js/vue-router-pjt/src/views/Rendering.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Rendering</h1>
+    <input type="text" v-model="type"><br>
+    <!-- if(가끔씩 사용한다)는 진짜 없는것, show(자주씩 사용한다)는 있는 display-none으로 해놓은것이다 -->
+    <h1 v-if="type==='A'">A</h1>
+    <h1 v-else-if="type==='B'">B</h1>
+    <h1 v-else>C</h1>
+    <hr>
+    <h1 v-show="type==='A'">A</h1>
+    <h1 v-show="type==='B'">B</h1>
+    <h1 v-show="type!=='A'&& type!=='B'">C</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            type:'A'
+        }
+    }
+}
+</script>
+
+<style>
+</style>
