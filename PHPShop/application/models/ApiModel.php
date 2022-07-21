bc1cd7478cca2536377baddd9c9e5e35e50a6bd6
yeongrong-heo <102986339+yeongr1203@users.noreply.github.com>
2022-07-21 18:15:20 +0900

    220721_수업

--- PHPShop/application/models/ApiModel.php
+++ PHPShop/application/models/ApiModel.php
@@ -36,6 +36,32 @@ class ApiModel extends Model {
         $stmt->execute();
         return $stmt->intval($this->lastInsertId());
     }
+    public function productList($param) {
+        $sql = "SELECT t1.*, t2.type, t2.path, t3.cate1, t3.cate2, t3.cate3
+                FROM t_product t1, t_product_img t2, t_category t3
+                WHERE t1.id = t2.product_id
+                AND t2.type = 1
+                AND t1.category_id = t3.id";
+
+        if(isset($param["cate3"])) {
+            $cate3 = $param["cate3"];
+            $sql .= " AND t3.id = {$cate3}";
+
+        } else {
+            if(isset($param["cate1"])) {
+                $cate1 = $param["cate1"];
+                $sql .= " AND t3.cate1 = '{$cate1}'";
+            }
+            if(isset($param["cate2"])) {
+                $cate2 = $param["cate2"];
+                $sql .= " AND t3.cate2 = '{$cate2}'";
+            }
+        }
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
 
     public function productList2() {
         $sql = 
@@ -48,7 +74,7 @@ class ApiModel extends Model {
                 LEFT JOIN (
                     SELECT * FROM t_product_img WHERE type = 1
                 ) t4
-                ON t3.id = t4.id
+                ON t3.id = t4.product_id
         ";
         $stmt = $this->pdo->prepare($sql);
         $stmt -> execute();
@@ -67,7 +93,7 @@ class ApiModel extends Model {
                 LEFT JOIN (
                     SELECT * FROM t_product_img WHERE type = 1
                 ) t4
-                ON t3.id = t4.id
+                ON t3.id = t4.product_id
         ";
         $stmt = $this->pdo->prepare($sql);
         $stmt -> bindValue(":product_id", $param["product_id"]);
@@ -105,28 +131,79 @@ class ApiModel extends Model {
         return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
 
-    // 상세페이지 안 등록된 이미지 삭제
-    public function porductImageDelete(&$param) {
-        $sql =
-        "   DELETE FROM t_product_img
-            WHERE id = :product_image_id        
-        ";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->bindValue(":product_image_id", $param["product_image_id"]);
+    public function productImageDelete(&$param) {
+        $sql = "DELETE FROM t_product_img WHERE ";
+        if(array_key_exists("product_image_id", $param)) {
+            $sql .= "id = " . $param["product_image_id"];
+
+        } else if (array_key_exists("product_id", $param)) {
+            $sql .= "product_id = " . $param["product_id"];
+        }        
+        $stmt = $this->pdo->prepare($sql);        
         $stmt->execute();
         return $stmt->rowCount();
     }
 
-    // 상세페이지 해당 img 선택해서 삭제.   -> 두번쨰방법.
-    public function productImgPath(&$param) {
-        $sql =
-        "   SELECT *
-            FROM t_product_img
-            WHERE id = :product_image_id
-        ";
+    // 상세페이지 안 등록된 이미지 삭제
+    public function productDelete(&$param) {
+        $sql = "DELETE FROM t_product
+            WHERE id = :product_id";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->bindValue(":product_image_id", $param["product_image_id"]);
+        $stmt->bindValue(":product_id", $param['product_id']);
         $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_OBJ);
+        return $stmt->rowCount();
     }
+
+    // 카테고리 설정.
+    // 전부 가져오면 되기 때문에 파라미터 필요 없음.
+    public function cate1List() {
+        $sql = "SELECT cate1 AS cate_nm FROM t_category
+                GROUP BY cate1 ORDER BY cate_nm";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function cate2List(&$param) {
+        $sql = "SELECT cate2 AS cate_nm
+                FROM t_category
+                WHERE cate1 = :cate1
+                GROUP BY cate2
+                ORDER BY cate_nm";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(":cate1", $param["cate1"]);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function cate3List(&$param) {
+        $sql = "SELECT id, cate3 AS cate_nm
+                FROM t_category
+                WHERE cate1 = :cate1
+                AND cate2 = :cate2
+                GROUP BY id, cate3
+                ORDER BY cate_nm";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(":cate1", $param["cate1"]);
+        $stmt->bindValue(":cate2", $param["cate2"]);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
 }
+
+
+    
+
+    // 상세페이지 해당 img 선택해서 삭제.   -> 두번쨰방법.
+    // public function productImgPath(&$param) {
+    //     $sql =
+    //     "   SELECT *
+    //         FROM t_product_img
+    //         WHERE id = :product_image_id
+    //     ";
+    //     $stmt = $this->pdo->prepare($sql);
+    //     $stmt->bindValue(":product_image_id", $param["product_image_id"]);
+    //     $stmt->execute();
+    //     return $stmt->fetch(PDO::FETCH_OBJ);
+    // }
